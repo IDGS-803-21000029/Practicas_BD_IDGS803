ee5c642e4e93205b592e1eaaff38242c19936123
IDGS-803-21000029 <21000029@alumnos.utleon.edu.mx>
2024-03-01 18:10:46 -0600

    JIGC 01.03.2024 ABC Terminado

--- forms.py
+++ forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, EmailField, IntegerField, DecimalField
 from wtforms import validators
 
 class EmployeeForm(Form):
+    id = IntegerField('id')
     nombre = StringField('Nombre', [validators.DataRequired(message='El campo es requerido'), validators.Length(min=4, max=25)])
     correo = EmailField('Email', [validators.DataRequired(message='Ingresa un Correo Valido')])
     telefono = IntegerField('Telefono', [validators.DataRequired(message='El campo es requerido'), validators.NumberRange(min=2, max=10000000000)])
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, Response
 import forms
 from flask_wtf.csrf import CSRFProtect
 from config import DevelopmentConfigMain
+from flask import redirect
 from models import db
 from models import Empleados
 
@@ -32,6 +33,55 @@ def index():
 def ABC_Completo():
     empleados = Empleados.query.all()
     return render_template("tblempleados.html", empleados=empleados)
+
+@app.route("/eliminar", methods=["GET", "POST"])
+def eliminar():
+    emp_form = forms.EmployeeForm(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        emp1 = db.session.query(Empleados).filter_by(id=id).first()
+        emp_form.id.data = request.args.get('id')
+        emp_form.nombre.data = emp1.nombre
+        emp_form.correo.data = emp1.correo
+        emp_form.telefono.data = emp1.telefono
+        emp_form.direccion.data = emp1.direccion
+        emp_form.sueldo.data = emp1.sueldo
+    elif request.method == 'POST':
+        id = emp_form.id.data
+        emp = Empleados.query.get(id)
+        db.session.delete(emp)
+        db.session.commit()
+        return redirect('/tblempleados')
+    
+    return render_template("eliminar.html", form=emp_form)
+
+@app.route("/modificar", methods=["GET", "POST"])
+def modificar():
+    emp_form = forms.EmployeeForm(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        emp1 = db.session.query(Empleados).filter_by(id=id).first()
+        emp_form.id.data = request.args.get('id')
+        emp_form.nombre.data = emp1.nombre
+        emp_form.correo.data = emp1.correo
+        emp_form.telefono.data = emp1.telefono
+        emp_form.direccion.data = emp1.direccion
+        emp_form.sueldo.data = emp1.sueldo
+    elif request.method == 'POST':
+        id = emp_form.id.data
+        emp = Empleados.query.get(id)
+        emp.nombre = emp_form.nombre.data
+        emp.correo = emp_form.correo.data
+        emp.telefono = emp_form.telefono.data
+        emp.direccion = emp_form.direccion.data
+        emp.sueldo = emp_form.sueldo.data
+        db.session.add(emp)
+        db.session.commit()
+        return redirect('/tblempleados')
+    
+    return render_template("modificar.html", form=emp_form)
+
+
 
 if __name__ == "__main__":
     csrf.init_app(app)
